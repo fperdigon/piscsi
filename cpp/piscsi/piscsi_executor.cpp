de6825b17e80da247ae222c0f515ec57da09e2b8
Uwe Seimet <Uwe.Seimet@seimet.de>
2023-10-07 18:31:18 +0200

    Cleanup

--- cpp/piscsi/piscsi_executor.cpp
+++ cpp/piscsi/piscsi_executor.cpp
@@ -111,7 +111,7 @@ bool PiscsiExecutor::ProcessCmd(const CommandContext& context)
 
 	// Remember the list of reserved files during the dry run
 	const auto& reserved_files = StorageDevice::GetReservedFiles();
-	const bool reserved = ranges::find_if_not(context.GetCommand().devices(), [&] (const auto& device)
+	const bool reserved = ranges::find_if_not(command.devices(), [&] (const auto& device)
 			{ return ProcessDeviceCmd(context, device, true); }) != command.devices().end();
 	StorageDevice::SetReservedFiles(reserved_files);
 	if (reserved) {
