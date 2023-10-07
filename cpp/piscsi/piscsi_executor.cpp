b357582864f3caf7fec7b58bd0fda396d6585101
Uwe Seimet <Uwe.Seimet@seimet.de>
2023-10-07 18:54:56 +0200

    Renaming

--- cpp/piscsi/piscsi_executor.cpp
+++ cpp/piscsi/piscsi_executor.cpp
@@ -110,11 +110,11 @@ bool PiscsiExecutor::ProcessCmd(const CommandContext& context)
 
 	// Remember the list of reserved files during the dry run
 	const auto& reserved_files = StorageDevice::GetReservedFiles();
-	const bool hasError = ranges::find_if_not(command.devices(), [&] (const auto& device)
+	const bool isDryRunError = ranges::find_if_not(command.devices(), [&] (const auto& device)
 			{ return ProcessDeviceCmd(context, device, true); }) != command.devices().end();
 	StorageDevice::SetReservedFiles(reserved_files);
 
-	if (hasError) {
+	if (isDryRunError) {
 		return false;
 	}
 
