3e41980427e2846132639dc805392c4fdc999cca
Uwe Seimet <Uwe.Seimet@seimet.de>
2023-10-10 16:26:59 +0200

    Cleanup

--- cpp/piscsi/piscsi_response.cpp
+++ cpp/piscsi/piscsi_response.cpp
@@ -35,11 +35,9 @@ void PiscsiResponse::GetDeviceProperties(const Device& device, PbDevicePropertie
 	properties.set_supports_file(device.SupportsFile());
 	properties.set_supports_params(device.SupportsParams());
 
-	if (device.SupportsParams()) {
-		for (const auto& [key, value] : device_factory.GetDefaultParams(device.GetType())) {
-			auto& map = *properties.mutable_default_params();
-			map[key] = value;
-		}
+	for (const auto& [key, value] : device_factory.GetDefaultParams(device.GetType())) {
+		auto& map = *properties.mutable_default_params();
+		map[key] = value;
 	}
 
 	for (const auto& block_size : device_factory.GetSectorSizes(device.GetType())) {
